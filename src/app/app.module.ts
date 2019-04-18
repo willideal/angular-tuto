c75a625e82db761e989ea8ab2228a4f9f972253a
Noutai Wilfrid Cesaire <wilfrid-cesaire.noutai@etu.uni-orleans.fr>
2019-04-18 20:34:30 +0200

    first commit tuto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule
